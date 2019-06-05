3bc2b506090cc84c5e59e9a932a9970011e15fda
phueb <phueb001@ucr.edu>
2019-06-05 10:17:25 -0500

    fix: calc_max_item_pp()

--- src/utils.py
+++ src/utils.py
@@ -11,21 +11,20 @@ def calc_max_cat_pp(input_params, num_sequences, num_types):
     # max_cat_pp is is cat_pp for punctuation
     # because the punctuation category only consists of 1 item and therefore has the least probability mass
     avg_window_size = int(input_params.punctuation) + 2 + np.mean([input_params.max_distance,
-                                                                      input_params.min_distance])
+                                                                   input_params.min_distance])
     num_windows = avg_window_size * num_sequences
     max_cat_pp = np.exp(-np.log(1 / num_types) * num_sequences / num_windows)
     return max_cat_pp
 
 
-def calc_max_item_pp(input_params):
+def calc_max_item_pp(input_params, cat):
     # max_item_pp is item_pp for the category which has largest set size
     # because least amount of probability mass is initially devoted to the correct item
     #
     assert input_params.min_distance == input_params.max_distance  # only works with this constraint
-    max_num_types = np.max([input_params.ab_cat_size,
-                            input_params.train_x_cat_size])
-    window_size = input_params.max_distance + 1 + 1 + 1
-    max_item_pp = np.exp(-np.log(1 / max_num_types) * (1 / window_size))
+    num_items_in_cat = input_params.ab_cat_size if cat in ['A', 'B'] else input_params.train_x_cat_size
+    window_size = input_params.max_distance + 2 + int(input_params.punctuation)
+    max_item_pp = np.exp(-np.log(1 / num_items_in_cat) * (1 / window_size))
     return max_item_pp
 
 
--- study_x_cat_size.py
+++ study_x_cat_size.py
@@ -2,24 +2,24 @@ import numpy as np
 import datetime
 import sys
 import pyprind
-from itertools import product
 
 from src.evaluation import check_item_pp_at_end
 from src.plotting import plot_pp_vs_x_cat_size
 from src.plotting import plot_params
 from src.jobs import train_loop
-from src.jobs import make_name2seqs
+from src.utils import calc_max_item_pp
 from src.corpus import AxbCorpus
 from src.vocab import Vocab
 from src.rnn import RNN
 from src import config
 
 TRAIN_DISTANCE = 1
-TRAIN_X_CAT_SIZES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+TRAIN_X_CAT_SIZES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 MAX_NUM_EPOCHS = 2
 NUM_REPS = 10
 PROGRESS_BAR = True
 LIMIT_BPPT = True  # if True, generalization to unseen distances is impossible
+MAX_NUM_SEQUENCES = 48  # total number of sequences in corpus with x_cat_size=24
 
 
 # params
@@ -42,19 +42,24 @@ for size_id, train_x_cat_size in enumerate(TRAIN_X_CAT_SIZES):
     setattr(input_params, 'test_x_cat_size', train_x_cat_size)
     setattr(input_params, 'min_distance', TRAIN_DISTANCE)
     setattr(input_params, 'max_distance', TRAIN_DISTANCE)
+    setattr(input_params, 'sample_size', MAX_NUM_SEQUENCES)
 
-    if LIMIT_BPPT:  # sets bptt to maximal bptt needed to learn training dependencies only
-        setattr(rnn_params, 'bptt', TRAIN_DISTANCE + 1)
-        print('Set bptt to {}'.format(rnn_params.bptt))
+    max_item_pp = calc_max_item_pp(input_params, 'B')
+    print('max_item_pp={}'.format(max_item_pp))
 
     # make train but not test sequences
     train_corpus = AxbCorpus(input_params, test=False)
     master_vocab = Vocab(train_corpus)
     name2seqs = {'train': master_vocab.generate_index_sequences(train_corpus)}
     seq_names = name2seqs.keys()
+    print('number of sequences in train corpus={}'.format(train_corpus.num_sequences))
 
     # set max epoch
     setattr(rnn_params, 'num_epochs', MAX_NUM_EPOCHS)
+
+    if LIMIT_BPPT:  # sets bptt to maximal bptt needed to learn training dependencies only
+        setattr(rnn_params, 'bptt', TRAIN_DISTANCE + 1)
+        print('Set bptt to {}'.format(rnn_params.bptt))
 
     # progressbar
     print('Training {} models...'.format(NUM_REPS))
@@ -87,8 +92,7 @@ for size_id, train_x_cat_size in enumerate(TRAIN_X_CAT_SIZES):
 # plot perplexity as function of x category size
 time_stamp = datetime.datetime.now().strftime("%B %d %Y %I:%M:%s")
 plot_params(time_stamp, input_params, rnn_params)
-plot_pp_vs_x_cat_size(item_pps_end, TRAIN_X_CAT_SIZES, 'train', 'Item')
+plot_pp_vs_x_cat_size(item_pps_end, TRAIN_X_CAT_SIZES, 'train', 'Item', y_max=max_item_pp)
 
 
     # TODO keep output size of model constant
-    # TODO keep total number of sequences constant: increase epochs when there are fewer than max amount of sequences
